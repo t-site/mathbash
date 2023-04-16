5dc9c676fee190619fa2ccf35e0df61f13f3f347
seitaro <sktruntime@hotmail.co.jp>
2023-04-16 15:54:53 +0900

    correct answers count added

--- powtest.sh
+++ powtest.sh
@@ -2,11 +2,15 @@
 
 RR="1*2="
 ANS="2"
+COUNT=0
+CORRECT=0
 
 while read -p "$RR" INPUT
 do
+	COUNT=$(( "${COUNT}" + 1 ))
 	if [[ "$ANS" == "$INPUT" ]]
 	then
+		CORRECT=$(( "${CORRECT}" + 1 ))
 		R1=$( shuf -n 1 99.txt )
 		R2=$( shuf -n 1 19.txt )
 		RR="$R1*$R2="
@@ -15,3 +19,5 @@ do
 		continue
 	fi
 done
+echo ''
+echo "${CORRECT} correct in ${COUNT}"
